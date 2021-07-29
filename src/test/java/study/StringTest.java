76a1f32681db213fee504edabb706502be7d982b
JongWoo Park <jjongwoo.dev@gmail.com>
2021-07-30 01:34:41 +0900

    0. 학습테스트 실습
    0.학습테스트 실습 - String 클래스에 대한 학습 테스트
    
    0.학습테스트 실습 - Set Collection에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,51 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void requirements1() {
+        // split
+        String[] values = "1,2".split(",");
+        assertThat(values).contains("1", "2");
+
+        values = "1".split(",");
+        assertThat(values).containsExactly("1");
+    }
+
+    @Test
+    void requirements2() {
+        // given
+        String input = "(1,2)";
+
+        // when
+        String output = input.substring(1, input.length() - 1);
+
+        // then
+        assertThat(output).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("Error handle하려고")
+    void requirements3() {
+        // given
+        String input = "abc";
+
+        // when
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    input.charAt(6);
+                });
+        // then
     }
 }
